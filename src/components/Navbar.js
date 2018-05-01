07e5f29c09959c52d31d8e1f46dda10b566a804a
Michael Dick <gritibaenz@gmail.com>
2018-05-01 23:10:32 +0200

    navigation, css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,44 +4,65 @@ import Link from 'gatsby-link'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo_neu_neu_neu.png'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent" style={{ height: '140px' }}>
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image" style={{width: 100 + 'px'}}>
-            <img src={logo} alt="Kaldi" style={{ maxHeight: '100%' }}/>
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/event">
-          Agenda
-        </Link>
-        <Link className="navbar-item" to="/anmeldung">
-          Anmeldung
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navToggle: false
+    };
+    
+    this.toggleNav = this.toggleNav.bind(this);
+  }
 
+  toggleNav() {
+    this.setState({ navToggle: !this.state.navToggle });
+    console.log(this.state.navToggle);
+  }
+
+  render() {
+    return (
+      <nav className="navbar is-transparent" style={{ height: '140px', zIndex:2 }}>
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure className="image" style={{ width: 160 + 'px' }}>
+                <img src={logo} alt="Kaldi" style={{ maxHeight: '100%' }} />
+              </figure>
+            </Link>
+            <a role="button" className={"navbar-burger " + (this.state.navToggle ? "is-active" : "")}
+              onClick={this.toggleNav} aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div className={"navbar-menu " + (this.state.navToggle ? "is-active" : "")} >
+            <div className="navbar-start has-text-centered is-size-5 has-text-weight-bold">
+              <Link className="navbar-item is-mobile" to="/event">
+                <p className="has-text-white"><i className="fa fa-calendar"></i>
+                  <br />Agenda</p>
+              </Link>
+              <Link className="navbar-item" to="/anmeldung">
+                <p className="has-text-white"><i className="fa fa-file-text-o"></i>
+                  <br />Anmeldung</p>
+              </Link>
+              <Link className="navbar-item" to="/about">
+                <p className="has-text-white"><i className="fa fa-info"></i>
+                  <br />About</p>
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                <p className="has-text-white"><i className="fa fa-envelope"></i>
+                  <br />Kontakt</p>
+              </Link>
+              <Link className="navbar-item" to="/products">
+                <p className="has-text-white"><i className="fa fa-question"></i>
+                  <br />Products</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
 export default Navbar
