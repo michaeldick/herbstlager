fadd2803fb951444bf9a3d3a7fd3793b7d587aaf
Michael Dick <gritibaenz@gmail.com>
2018-05-03 21:46:33 +0200

    formtest

---
+++ src/pages/formtest.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Formtest = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <form
+      name="contact-form"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input name="name" placeholder="Your Name" type="text" />
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
+      <button>Send</button>
+    </form>
+  </div>
+)
+
+export default Formtest
