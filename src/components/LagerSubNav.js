4060c534e02569e9aea955df67dddcd1ed17b7f8
Michael Dick <gritibaenz@gmail.com>
2018-09-09 23:37:07 +0200

    buttons

--- src/components/LagerSubNav.js
+++ src/components/LagerSubNav.js
@@ -6,9 +6,9 @@ const LagerSubNav = ({ }) => (
   <div className="full-width-container">
     <div className="columns is-mobile">
       {/* <div className="column has-text-centered"><Link to="/anmeldung"><p className="button is-primary">Anmeldung</p></Link></div> */}
-      <div className="column has-text-centered"><Link to="/#"><p className="button is-primary" disabled>Tagebuch</p></Link></div>
+      <div className="column has-text-centered"><Link to="#"><p className="button is-primary" disabled>Tagebuch</p></Link></div>
       <div className="column has-text-centered"><Link to="/lager2018team"><p className="button is-primary">Lagerleitung</p></Link></div>
-      <div className="column has-text-centered"><Link to="/img/Hela_2018_Lagerbuechli.pdf"><p className="button is-primary">Lagerb&uuml;echli</p></Link></div>
+      <div className="column has-text-centered"><Link to="/img/Hela_2018_Lagerbuechli.pdf" target="_blank"><p className="button is-primary">Lagerb&uuml;echli</p></Link></div>
     </div>
   </div>
 )
