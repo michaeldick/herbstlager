c01a7f28825324c0f6bfe67cb768029fe35da2a7
Michael Dick <mdi@softwaremanufaktur.ch>
2018-09-29 00:51:18 +0200

    navigation

--- src/components/LagerSubNav.js
+++ src/components/LagerSubNav.js
@@ -6,7 +6,7 @@ const LagerSubNav = ({ }) => (
   <div className="full-width-container">
     <div className="columns is-mobile">
       {/* <div className="column has-text-centered"><Link to="/anmeldung"><p className="button is-primary">Anmeldung</p></Link></div> */}
-      <div className="column has-text-centered"><a href="#" disabled><p className="button is-primary" disabled>Tagebuch</p></a></div>
+      <div className="column has-text-centered"><a href="/tagebuch2018"><p className="button is-primary">Tagebuch</p></a></div>
       <div className="column has-text-centered"><Link to="/lager2018team"><p className="button is-primary">Lagerleitung</p></Link></div>
       <div className="column has-text-centered"><a href="/img/Hela_2018_Lagerbuechli.pdf" target="_blank"><p className="button is-primary">Lagerb&uuml;echli</p></a></div>
     </div>
